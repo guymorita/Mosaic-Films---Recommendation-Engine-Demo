8c5733acefbca5e99dce50409e45de33a86287a8
Guy Morita <guymorita@gmail.com>
2013-08-06 15:27:43 -0700

    updated node server. now only has two primary api's

--- node-server.js
+++ node-server.js
@@ -1,7 +1,4 @@
-// config
 urlOfDB = 'mongodb://localhost/users';
-// url of csv file
-
 
 var express = require('express'),
     raccoon = require('raccoon').raccoon(urlOfDB),
@@ -16,48 +13,26 @@ app.get('/login', function(req, res){
   });
 });
 
+app.get('/newRating', function(req, res){
+  if (req.query.movie.like === 'liked'){
+    raccoon.input.liked(req.query[':userId'], req.query.movie.id, function(){
+      raccoon.stat.recommendFor(req.query[':userId'], 30, function(recs){
+        console.log('recs', recs);
+        res.send(recs);
+      });
+    });
+  } else {
+    raccoon.input.disliked(req.query[':userId'], req.query.movie.id, function(){
+      raccoon.stat.recommendFor(req.query[':userId'], 30, function(recs){
+        res.send(recs);
+      });
+    });
+  }
+});
+
 app.get('/importMovies', function(req, res){
   raccoon.models.importCSV(raccoon.models.importLib);
   res.send('SUCCESS: Movies Imported');
-});
-
-app.get('/recommend', function(req, res){
-  console.log('query', req.query[':id']);
-  // console.log(req.path);
-  // console.log('req query', req.query[':name']);
-  // console.log(raccoon.models.useee, req.query[':id']);
-  raccoon.stat.likedCount(req.query[':id'], function(rec){
-    console.log('rec', rec);
-    res.send(rec);
-  });
-});
-
-app.get('/topSimilarUsers', function(req, res){
-  raccoon.topSimilarUsers(raccoon.models.useee, req.query[':name'], function(rec){
-    res.send(rec);
-  });
-});
-
-app.get('/getMovieList', function(req, res){
-  res.send(raccoon.models.movee);
-});
-
-app.post('/newUser', function(req, res){
-  var chunk = '';
-  req.on('data', function(data){
-    chunk += data;
-  });
-  req.on('end', function(){
-    chunk = JSON.parse(chunk);
-    raccoon.models.buildMovieList(chunk, function(movieList){
-      raccoon.models.newUser(chunk.name, movieList, function(){
-        raccoon.models.importLib(function(){
-          res.send(204);
-        });
-      });
-    });
-  });
-  // console.log(res);
 });
 
 app.listen(3000);
